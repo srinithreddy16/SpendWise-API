59143ef32ee28009a27b484604c16fc898d3ba8f
Srinith Reddy <srinithreddy2002@gmail.com>
2026-02-09 14:10:16 -0600

    Project Setup

---
+++ src/main/java/com/spendwise/SpendwiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.spendwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendwiseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendwiseApiApplication.class, args);
+    }
+}
+
---
+++ src/main/java/com/spendwise/domain/entity/package-info.java
@@ -0,0 +1,2 @@
+package com.spendwise.domain.entity;
+
